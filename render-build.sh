35224080698f2d1ed76286d53101f333ba3099d2
KrxTrends <krxtrends@gmail.com>
2025-03-15 15:46:18 +0000

    modified the bash file

--- render-build.sh
+++ render-build.sh
@@ -7,13 +7,15 @@ set -o errexit
 mkdir -p /opt/render/project/.render/chrome
 
 # Download and install Chrome
+echo "Downloading and Installing Chrome from https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 curl -SL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/chrome.deb
 dpkg -x /tmp/chrome.deb /opt/render/project/.render/chrome
 
 # Download and install ChromeDriver
 CHROME_VERSION=$(/opt/render/project/.render/chrome/opt/google/chrome/google-chrome --version | awk '{print $3}')
-CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 # CHROME_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+echo "Downloding and installing chrome driver from https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
 curl -SL "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip" -o /tmp/chromedriver.zip
 unzip /tmp/chromedriver.zip -d /opt/render/project/.render/chrome
 
